32245aa3cb65d0b9893fed10fcc836ee388dcde1
Harshit <harshit.agrawal1990@gmail.com>
2025-03-18 11:29:35 +0000

    second commit

---
+++ src/test/java/testCases/TC03_LoginTest.java
@@ -0,0 +1,24 @@
+package testCases;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.MyAccountPage;
+import testBase.BaseClass;
+
+public class TC03_LoginTest extends BaseClass{
+
+
+public static void main(String[] args) {
+	
+	System.out.println("test");
+	
+}
+}
+		
+		
+	
